827ae08dc41f7613aa715a8eb6ef48652cdd95bc
fambrogl <sha1-e692d0cc07db26fe72a735bf800f9d70a3ebe9a2@cern.ch>
2009-09-22 13:44:16 +0000

    update the confg file for test the geometry

--- test/testDTGeometry_cfg.py
+++ test/testDTGeometry_cfg.py
@@ -1,23 +1,12 @@
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("GeometryTest")
-process.load("Geometry.MuonNumbering.muonNumberingInitialization_cfi")
-process.load("Geometry.MuonCommonData.muonIdealGeometryXML_cfi")
-
-process.load("Geometry.DTGeometry.dtGeometry_cfi")
-process.DTGeometryESModule.fromDDD = cms.bool(True)
-
+process.load('Configuration/StandardSequences/GeometryExtended_cff')
 process.load('Configuration/StandardSequences/FrontierConditions_GlobalTag_cff')
-process.GlobalTag.globaltag = 'IDEAL_31X::All' 
-#process.load("Configuration.StandardSequences.FakeConditions_cff")
 
+process.GlobalTag.globaltag = 'MC_31X_V8::All'
 process.load("Alignment.CommonAlignmentProducer.FakeAlignmentSource_cfi")
 process.preferFakeAlign = cms.ESPrefer("FakeAlignmentSource") 
-
-process.load("CondCore.DBCommon.CondDBCommon_cfi")
-
-#process.load("Geometry.CMSCommonData.cmsIdealGeometryXML_cfi")
-
 
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(1)
@@ -30,6 +19,5 @@ process.out = cms.OutputModule("AsciiOutputModule")
 process.prod = cms.EDAnalyzer("DTGeometryAnalyzer")
 
 process.p1 = cms.Path(process.prod)
-#process.e = cms.EndPath(process.out)
 
 
